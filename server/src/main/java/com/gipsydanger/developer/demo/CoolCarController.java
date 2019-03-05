48f4e2bb7b21fac8b57b92b40a4f4c2f5f92de7c
padovese <bhspadoveze@gmail.com>
2019-03-05 01:18:31 -0300

    allowing cross origin access

--- server/src/main/java/com/gipsydanger/developer/demo/CoolCarController.java
+++ server/src/main/java/com/gipsydanger/developer/demo/CoolCarController.java
@@ -3,6 +3,7 @@ package com.gipsydanger.developer.demo;
 import java.util.Collection;
 import java.util.stream.Collectors;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,6 +18,7 @@ public class CoolCarController {
     }
 
     @GetMapping("/cool-cars")
+    @CrossOrigin(origins = "http://localhost:4200")
     public Collection<Car> coolCars() {
         return repository.findAll().stream()
                 .filter(this::isCool)
